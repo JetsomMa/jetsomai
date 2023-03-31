19e1ccf42d026b65d2361fefc61f230483638377
mashaojie <mashaojie@pinefield-inc.com>
2023-03-31 19:23:55 +0800

    perf: 项目初始化成功

---
+++ code/data_import/README.md
@@ -0,0 +1,13 @@
+# 数据导入至向量数据库
+
+## 安装依赖
+
+`pip install -r requirements.txt`
+
+## 设置OPENAI_API_KEY
+
+`export OPENAI_API_KEY=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`
+
+## 运行
+
+`python import_data.py`
---
+++ code/data_import/import_data.py
@@ -0,0 +1,48 @@
+from qdrant_client import QdrantClient
+from qdrant_client.http.models import Distance, VectorParams
+from qdrant_client.http.models import PointStruct
+import os
+import tqdm
+import openai
+import json
+import time
+
+collection_name = "pinefield_data_collection_test"
+
+def to_embeddings(prompt):
+    # 休眠1秒
+    time.sleep(1)
+    sentence_embeddings = openai.Embedding.create(
+        model="text-embedding-ada-002",
+        input=prompt
+    )
+    return sentence_embeddings["data"][0]["embedding"]
+
+
+if __name__ == '__main__':
+    client = QdrantClient("127.0.0.1", port=6333)
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    # 创建collection
+    client.recreate_collection(
+        collection_name,
+        vectors_config=VectorParams(size=1536, distance=Distance.COSINE),
+    )
+
+    count = 0
+    for root, dirs, files in os.walk("./source_data"):
+        for file in files:
+            file_path = os.path.join(root, file)
+            with open(file_path, 'r', encoding='utf-8') as f:
+                train_data_org = json.load(f)
+                for item in tqdm.tqdm(train_data_org):
+                    vector = to_embeddings(item['prompt'])
+                    payload={"title": item['prompt'], "text": item['completion']}
+                    # print(payload)
+                    client.upsert(
+                        collection_name,
+                        wait=True,
+                        points=[
+                            PointStruct(id=count, vector=vector, payload=payload),
+                        ],
+                    )
+                    count += 1
